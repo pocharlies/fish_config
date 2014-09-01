0b4d0cecfa011bbf3518601052c25e05834330d9
Daniel Ibáñez <dibanez@grupoblidoo.com>
2014-09-01 09:46:56 +0200

    Modified the date to the beginning

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -57,7 +57,7 @@ function fish_prompt --description 'Write out the prompt'
 
    set -g __fish_prompt_date (date +%X)
 
-   echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" ' at ' "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' in ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$__fish_prompt_date_color" ' [' "$__fish_prompt_date" ']'
+   echo -n -s "$__fish_prompt_date_color" '[' "$__fish_prompt_date" ']' ' ' "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" ' at ' "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' in ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status"
     echo
     printf '↪ '
 end
