683dd9b23ce628db2c9e5e2bb737d156263693bb
Daniel Ibáñez <dibanez@grupoblidoo.com>
2014-09-15 17:15:58 +0200

    RVM Does not work!

--- config.fish
+++ config.fish
@@ -1,4 +1,1 @@
 set -g -x PATH /usr/local/bin $PATH
-if test -z $rvm_bin_path
-  exec bash --login -c "exec fish"
-end
--- functions/rvm.fish
+++
@@ -1,3 +0,0 @@
-function rvm --description "Ruby Version Manager"
-  exec bash --login -c "rvm $argv; exec fish"
-end
